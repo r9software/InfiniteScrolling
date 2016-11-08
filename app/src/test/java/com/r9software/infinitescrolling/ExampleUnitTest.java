5c33efc5bff1ee1c718f1f155ff86d5857a71fd0
Rodolfo Abarca <r9dolfinho@hotmail.com>
2016-11-07 21:12:44 -0600

    added Robolectric test

--- app/src/test/java/com/r9software/infinitescrolling/ExampleUnitTest.java
+++ app/src/test/java/com/r9software/infinitescrolling/ExampleUnitTest.java
@@ -1,13 +1,24 @@
 package com.r9software.infinitescrolling;
 
+import android.app.Activity;
+import android.view.Menu;
+
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
 
 import static org.junit.Assert.*;
+import static org.robolectric.Shadows.shadowOf;
 
 
+@RunWith(RobolectricGradleTestRunner.class)
+@Config(constants = BuildConfig.class)
 public class ExampleUnitTest {
-    @Test
-    public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
+
+        @Test
+        public void onCreateTest() {
+
+        }
     }
-}
